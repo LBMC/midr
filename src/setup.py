16f1d0915b3d6445b6b7e45677a4b54943a27c2e
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2019-09-05 16:41:04 +0200

    setup.py: change version from 1.0.2 to 1.0.3

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ with open("../README.md", "r") as fh:
 
 setuptools.setup(
     name="midr",
-    version="1.0.2",
+    version="1.0.3",
     packages=['midr'],
     install_requires=[
         'scipy>=1.3',
