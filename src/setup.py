a35c159c851decf93b36084d0e30e97f2873f75d
Laurent Modolo <laurent@modolo.fr>
2020-02-14 17:06:58 +0100

    setup.py: change package version

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="midr",
-    version="1.1.0",
+    version="1.2.0",
     packages=['midr'],
     install_requires=[
         'scipy>=1.3',
